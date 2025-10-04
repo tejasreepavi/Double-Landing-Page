a3bbe28f13ca1cc5ba7ffdf10f944777f1040529
tejasreepavi <tejasreepavi@gmail.com>
2025-10-04 21:36:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const vivoImg = document.getElementById('vivoImage');
-
-vivoImg.addEventListener('click', () => {
-  vivoImg.classList.toggle('zoomed');
+// Select all anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
 });
