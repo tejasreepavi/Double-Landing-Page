3821aa51ec798adb5082b4bbef1b4b3a8ea5fbb3
tejasreepavi <tejasreepavi@gmail.com>
2025-10-04 21:54:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-// Smooth scrolling for all navbar links
-document.querySelectorAll('.navbar a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href').substring(1);
-    const targetSection = document.getElementById(targetId);
-    if(targetSection) {
-      targetSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
+const containerEl = document.querySelector(".container");
+
+const leftEl = document.querySelector(".left");
+const rightEl = document.querySelector(".right");
+
+leftEl.addEventListener("mouseenter", () => {
+  containerEl.classList.add("active-left");
+});
+
+leftEl.addEventListener("mouseleave", () => {
+  containerEl.classList.remove("active-left");
+});
+
+rightEl.addEventListener("mouseenter", () => {
+  containerEl.classList.add("active-right");
+});
+
+rightEl.addEventListener("mouseleave", () => {
+  containerEl.classList.remove("active-right");
+});
